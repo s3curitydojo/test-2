57f64828e8f3ce4547e852059e2eacafa93fdf71
Your Name <you@example.com>
2023-07-25 04:27:53 +0530

    Testing TF plan for template instance

---
+++ 990/template_instance003.tf
@@ -0,0 +1,24 @@
+# This file is for demonstrating that apply_on_plan.sh is possible by generating an S3 bucket
+resource "random_string" "random_name" {
+  length    = 8
+  min_lower = 8
+}
+
+resource "aws_s3_bucket_acl" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "remove-this-test-bucket-${random_string.random_name.result}"
+
+  tags = {
+    Name        = "Remove-This"
+    Environment = "Test-Env"
+  }
+}
+
+output "bucket_name" {
+  description = "The name of the S3 bucket"
+  value       = aws_s3_bucket.bucket.id
+}
